4dd60246d587bd0981cdd4d50a1a9d82550e7148
uu_nur <1251623379@qq.com>
2020-07-03 14:56:13 +0800

    celery periodic task

--- celery_app/celeryconfig.py
+++ celery_app/celeryconfig.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+from celery.schedules import crontab
+
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 
@@ -8,3 +11,17 @@ CELERY_IMPORTS = (
     'celery_app.task1',
     'celery_app.task2'
 )
+
+# 定时任务
+CELERYBEAT_SCHEDULE = {
+    'add-every-30-seconds':{
+        'task':'celery_app.task1.add',
+        'schedule': timedelta(seconds=30),
+        'args': (5, 8)
+    },
+    'multiply-at-some-time':{
+        'task':'celery_app.task2.multiply',
+        'schedule': crontab(hour=15,minute=15),
+        'args': (3, 7)
+    }
+}
